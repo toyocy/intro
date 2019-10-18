e6dae7f86cdff3886ed04360c0587b4ea949a15b
Taka, Toyoshima <oq0q6383qqs@gmail.com>
2019-10-18 17:31:58 +0900

    scaffolding user

---
+++ db/migrate/20191018083141_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :last_name
+      t.string :first_name
+      t.string :email
+      t.boolean :is_active
+      t.references :organization, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
