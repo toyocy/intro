e4fd3f8155e3a9543c3a7ea432be478183982cb0
Taka, Toyoshima <oq0q6383qqs@gmail.com>
2019-10-24 01:28:54 +0900

    add landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   resources :users
   resources :organizations
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
